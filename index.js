f32633db05bc5dfc7949ada2a1777506ea8e513d
Mosey <moseev.es@ya.ru>
2021-02-11 22:17:47 +0300

    solve

--- index.js
+++ index.js
@@ -1,23 +1,54 @@
 const http = require('http');
 const moment = require('moment');
 const server = http.createServer();
+const fs = require('fs');
 
 server.listen(process.env.PORT || 80);
 
-server.on('request', (req, res) => {
+server.on('request', ({ url }, res) => {
 
-  if (req.url.startsWith('/add?')) {
-    const query_string = req.url.split('?')[1];
-    let answer_arr = [];
-    let amount = 0;
-    query_string.split('&').forEach(equality => {
-      let [key, value] = equality.split('=');
-      answer_arr.push(key);
-      amount += parseInt(value);
-    });
-    const answer = answer_arr.join(' + ') + ' = ' + amount;
-    res.end(answer);
+  if (url.startsWith('/add?')) {
+
+    res.end(solve(url, '+'));
+
+  } else if (url.startsWith('/deduct?')) {
+
+    res.end(solve(url, '-'));
+
+  } else if (url.startsWith('/mpy?')) {
+
+    res.end(solve(url, '*'));
+
+  } else if (url.startsWith('/del?')) {
+
+    res.end(solve(url, '/'));
+
+  } else if (url.startsWith('/up?')) {
+
+    res.end(solve(url, '**'));
+
+  } else if (url.startsWith('/code')) {
+
+    fs.createReadStream('./index.js').pipe(res);
+
+  } else {
+
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+    res.end(JSON.stringify({ date: moment().format('DD.MM.YYYY HH:mm:ss') }));
+
   }
-  res.setHeader('Content-Type', 'application/json; charset=utf-8');
-  res.end(JSON.stringify({ date: moment().format('DD.MM.YYYY HH:mm:ss') }));
 });
+
+function solve(query, char) {
+  const query_string = query.split('?')[1];
+  const key_arr = [];
+  const val_arr = [];
+
+  query_string.split('&').forEach(equality => {
+    const [key, value] = equality.split('=');
+    key_arr.push(key);
+    val_arr.push(value);
+  });
+  const result = eval(val_arr.join(char));
+  return key_arr.join(` ${char} `) + ' = ' + result;
+}
