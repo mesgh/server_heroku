9350954e657e9e3dbf527291226a2c71f40bbf89
Mosey <moseev.es@ya.ru>
2021-02-11 21:05:40 +0300

    add v.2

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 require('http')
 .Server((req, res) => {
-  if (req.url === '/add'){
-    res.end('HELLO!')
+
+  if (req.url.startsWith('/add?')) {
+    const query_string = req.url.split('?')[1];
+    let answer_arr = [];
+    let amount = 0;
+    query_string.split('&').forEach( equality => {
+      let [key, value] = equality.split('=');
+      answer_arr.push(key);
+      amount += value;
+    });
+    const answer = answer_arr.join(' + ') + ' = ' + amount;
+    res.end(answer);
   }
   res.writeHead(200, {
     'Content-Type': 'text/javascript; charset=utf-8'
