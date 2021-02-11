df17e7d2a3eeaebe3e888bd8360713ad44210551
Mosey <moseev.es@ya.ru>
2021-02-11 20:44:33 +0300

    /add

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 require('http')
-.Server((req, res) => res.end('HELLO!'))
+.Server((req, res) => {
+  if (req.url === '/add'){
+    res.end('HELLO!')
+  }
+  res.writeHead(200, {
+    'Content-Type': 'text/javascript; charset=utf-8'
+  });
+  require('fs').createReadStream('./index,js').pipe(res);
+})
 .listen(process.env.PORT || 80)
